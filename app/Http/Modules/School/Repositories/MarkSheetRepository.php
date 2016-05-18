9297f63794cbb8aa88bb4c46bad803d9882ba473
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-05-19 02:49:25 +0300

    Add sprocs

--- app/Http/Modules/School/Repositories/MarkSheetRepository.php
+++ app/Http/Modules/School/Repositories/MarkSheetRepository.php
@@ -24,79 +24,51 @@ class MarkSheetRepository extends Repository
         );
     }
 
+    public function unlock($model)
+    {
+        $procedure = 'sproc_sch_mark_sheet_unlock_dml';
 
-    // public function insert($model)
-    // {
-    //     $procedure = 'sproc_prd_product_dml_ins';
+        $iparams = [
+            ':iparam_exam_entity_id',
+            ':iparam_class_entity_id',
+            ':iparam_subject_entity_id',
+            ':iparam_session_id',
+            ':iparam_user_id',
+            ':iparam_screen_id',
+            ':iparam_debug_sproc',
+            ':iparam_audit_screen_visit',
+        ];
 
-    //     $iparams = [
-    //         ':iparam_product_name',
-    //         ':iparam_product_type_entity_id',
-    //         ':iparam_unit_rate',
-    //         ':iparam_facility_ids',
-    //         ':iparam_session_id',
-    //         ':iparam_user_id',
-    //         ':iparam_screen_id',
-    //         ':iparam_debug_sproc',
-    //         ':iparam_audit_screen_visit',
-    //     ];
+        $oparams = [
+            '@oparam_err_flag',
+            '@oparam_err_step',
+            '@oparam_err_msg'
+        ];
 
-    //     $oparams = [
-    //         '@oparam_product_entity_id',
-    //         '@oparam_err_flag',
-    //         '@oparam_err_step',
-    //         '@oparam_err_msg'
-    //     ];
+        return $this->runProcedure($model, $procedure, $iparams, $oparams);
+    }
 
-    //     return $this->runProcedure($model, $procedure, $iparams, $oparams);
-    // }
+    public function lock($model)
+    {
+        $procedure = 'sproc_sch_mark_sheet_lock_dml';
 
-    // public function update($model)
-    // {
-    //     $procedure = 'sproc_prd_product_dml_upd';
+        $iparams = [
+            ':iparam_exam_entity_id',
+            ':iparam_class_entity_id',
+            ':iparam_subject_entity_id',
+            ':iparam_session_id',
+            ':iparam_user_id',
+            ':iparam_screen_id',
+            ':iparam_debug_sproc',
+            ':iparam_audit_screen_visit',
+        ];
 
-    //     $iparams = [
-    //         ':iparam_product_entity_id',
-    //         ':iparam_product_name',
-    //         ':iparam_product_type_entity_id',
-    //         ':iparam_unit_rate',
-    //         ':iparam_active',
-    //         ':iparam_facility_ids',
-    //         ':iparam_session_id',
-    //         ':iparam_user_id',
-    //         ':iparam_screen_id',
-    //         ':iparam_debug_sproc',
-    //         ':iparam_audit_screen_visit',
-    //     ];
+        $oparams = [
+            '@oparam_err_flag',
+            '@oparam_err_step',
+            '@oparam_err_msg'
+        ];
 
-    //     $oparams = [
-    //         '@oparam_err_flag',
-    //         '@oparam_err_step',
-    //         '@oparam_err_msg'
-    //     ];
-
-    //     return $this->runProcedure($model, $procedure, $iparams, $oparams);
-    // }
-
-    // public function delete($model)
-    // {
-    //     $procedure = 'sproc_prd_product_dml_del';
-
-    //     $iparams = [
-    //         ':iparam_product_entity_id',
-    //         ':iparam_session_id',
-    //         ':iparam_user_id',
-    //         ':iparam_screen_id',
-    //         ':iparam_debug_sproc',
-    //         ':iparam_audit_screen_visit',
-    //     ];
-
-    //     $oparams = [
-    //         '@oparam_err_flag',
-    //         '@oparam_err_step',
-    //         '@oparam_err_msg'
-    //     ];
-
-    //     return $this->runProcedure($model, $procedure, $iparams, $oparams);
-    // }
+        return $this->runProcedure($model, $procedure, $iparams, $oparams);
+    }
 }
