ab6142ec6e6612eb2b12262144b800f892fd702c
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-05-19 17:13:11 +0300

    Fix

--- resources/views/layout/cabinet.blade.php
+++ resources/views/layout/cabinet.blade.php
@@ -3,8 +3,8 @@
 <head>
     @include('include.head')
     @yield('styles')
-    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
+<!--     <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" /> -->
 </head>
 <body>
 <div class="container-fluid">
@@ -35,9 +35,9 @@
 
 
  <!-- <script type="text/javascript" src="/bower_components/jquery/jquery.min.js"></script> -->
- <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
+ <!-- <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script> -->
  <!-- <script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
- <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+ <!-- <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script> -->
 
 <script>
     // for tables
