9cd32fa3bc6d4181b357ca9af567fd58b8d24be8
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-05-27 22:06:31 +0300

    pdf

---
+++ resources/views/reports/common/css.blade.php
@@ -0,0 +1,31 @@
+<style type="text/css">
+    .header-table {
+        width: 100%;
+        border: 0px;
+    }
+    .school-name {
+        margin-bottom: 5px;
+    }
+    .school-phone {
+        margin-top: 5px;
+    }
+    .report-name {
+        margin-top: 15px;
+        font-size: 21px;
+    }
+    p { page-break-after: always; }
+    .footer {
+        position: fixed;
+        bottom: 15px;
+        right: 15px;
+    }
+    .pagenum:before {
+        content: counter(page);
+    }
+    .tr h4, .tr h3 {
+        vertical-align:middle;
+    }
+    .pdf-page-break {
+        page-break-after: always;
+    }
+</style>
