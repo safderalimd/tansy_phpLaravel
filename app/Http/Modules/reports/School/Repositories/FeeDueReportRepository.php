b9d869a759c340f333dfde151cace4c5fd065947
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-08 15:58:56 +0300

    fix

--- app/Http/Modules/reports/School/Repositories/FeeDueReportRepository.php
+++ app/Http/Modules/reports/School/Repositories/FeeDueReportRepository.php
@@ -6,6 +6,20 @@ use App\Http\Repositories\Repository;
 
 class FeeDueReportRepository extends Repository
 {
+    public function getAccountsDropdown()
+    {
+        return $this->select(
+            'SELECT
+                row_type,
+                entity_id AS primary_key_id,
+                drop_down_list_name,
+                sequence_id,
+                reporting_order
+             FROM view_lkp_account_type_filter
+             ORDER BY sequence_id, reporting_order ASC;'
+        );
+    }
+
     public function getAllFees($model)
     {
         $procedure = 'sproc_act_rcv_due_lst';
--- resources/views/reports/school/FeeDueReport/list.blade.php
+++ resources/views/reports/school/FeeDueReport/list.blade.php
@@ -22,7 +22,7 @@
                     <label class="control-label" for="primary_key_id">Account Type</label>
                     <select id="primary_key_id" class="form-control" name="pk">
                         <option data-rowType="none" value="none">Select an account</option>
-                        @foreach($report->accountType4ReceivablePayment() as $option)
+                        @foreach($report->accountsDropdown() as $option)
                             <option data-rowType="{{$option['row_type']}}" value="{{ $option['primary_key_id'] }}">{{ $option['drop_down_list_name'] }}</option>
                         @endforeach
                     </select>
