dced137b2a8c3faf79696602604e4095eaa980b1
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-13 15:59:56 +0300

    fix

--- resources/views/modules/school/Admission/form.blade.php
+++ resources/views/modules/school/Admission/form.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Admission')
 
 @section('content')
-    {{d($admission)}}
     <div class="row">
         <div class="col-md-8 sch_class panel-group panel-bdr">
             <div class="panel panel-primary">
