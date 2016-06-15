cbafb2728b5725a5d964ed8aad9553a0fc095e30
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-15 16:38:42 +0300

    fix

--- app/Http/Modules/reports/School/Controllers/StudentDetailController.php
+++ app/Http/Modules/reports/School/Controllers/StudentDetailController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Modules\reports\School\Models\StudentDetail;
 use App\Http\PdfGenerator\Pdf;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class StudentDetailController extends Controller
 {
@@ -31,6 +33,22 @@ class StudentDetailController extends Controller
         $export = new StudentDetail($request->input());
         $export->loadPdfData();
         $view = view('reports.school.StudentDetail.pdf', compact('export'));
-        return Pdf::render($view);
+        // return Pdf::render($view);
+
+        $html = $view->render();
+
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'letter');
+        $dompdf->render();
+        $dompdf->stream();
+
+        // $output = $dompdf->output();
+        // return response($output)
+        //     ->header('Content-Type', 'application/pdf')
+        //     ->header('Content-Disposition', 'inline; filename="report.pdf')
+        //     ->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+        //     ->header('Pragma', 'no-cache')
+        //     ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
     }
 }
