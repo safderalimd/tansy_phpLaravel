558a844e84185b64b132bd768aa82570c5b98440
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-16 01:31:55 +0300

    fix

--- resources/views/reports/school/ProgressPrintClass/pdf.blade.php
+++ resources/views/reports/school/ProgressPrintClass/pdf.blade.php
@@ -34,8 +34,8 @@
             <div class="col-md-12">
                 <table class="header-table">
                     <tr>
-                        <td class="text-left"><h4 class="">Date: {{current_date()}}</h4></td>
-                        <td class="text-right"><h4 class="">Time: {{current_time()}}</h4></td>
+                        <td class="text-left"><h4 class="">Print Date: {{current_date()}}</h4></td>
+                        <td class="text-right"><h4 class="">Print Time: {{current_time()}}</h4></td>
                     </tr>
                 </table>
             </div>
