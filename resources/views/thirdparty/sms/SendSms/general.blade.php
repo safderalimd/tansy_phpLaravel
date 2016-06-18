6fae8e97cf580eca21f492246463bbded3b17855
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-18 18:43:33 +0300

    Select to sms general

--- resources/views/thirdparty/sms/SendSms/general.blade.php
+++ resources/views/thirdparty/sms/SendSms/general.blade.php
@@ -19,7 +19,7 @@
                 <label class="col-xs-3 col-md-2 control-label" for="sms_type_id">SMS Type</label>
                 <div class="col-xs-9 col-md-3">
                     <select id="sms_type_id" class="form-control" name="sms_type_id">
-                        <option value="none">Select a sms type</option>
+                        <option value="none">Select a sms type..</option>
                         @foreach($sms->generalSmsTypes as $option)
                             <option {{ activeSelect($option['sms_type_id'], 'sti') }} value="{{ $option['sms_type_id'] }}">{{ $option['sms_type'] }}</option>
                         @endforeach
@@ -37,7 +37,7 @@
                 <label class="col-xs-3 col-md-2 control-label" for="sms_account_entity_id">Filter Accounts</label>
                 <div class="col-xs-9 col-md-3">
                     <select id="sms_account_entity_id" class="form-control" name="sms_account_entity_id">
-                        <option value="none">Select an account</option>
+                        <option value="none">Select an account..</option>
                         @foreach($sms->smsAccountTypes as $option)
                             <option data-rowType="{{$option['row_type']}}" {{activeSelectByTwo($option['entity_id'], $option['row_type'], 'aei', 'art')}} value="{{ $option['entity_id'] }}">{{ $option['drop_down_list_name'] }}</option>
                         @endforeach
