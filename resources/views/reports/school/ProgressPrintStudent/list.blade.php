36f45775ba135fcc68d986e285cf7f87868f014e
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-21 17:36:08 +0300

    Add UI validation for progress student

--- resources/views/reports/school/ProgressPrintStudent/list.blade.php
+++ resources/views/reports/school/ProgressPrintStudent/list.blade.php
@@ -14,28 +14,38 @@
 
             @include('commons.errors')
 
-            <form class="form-inline" id="generate-report-form" action="/cabinet/progress-print--student/pdf" target="_blank" method="GET">
+            <form class="form-horizontal" id="generate-report-form" action="/cabinet/progress-print--student/pdf" target="_blank" method="GET">
                 <input type="hidden" id="random_id" name="ri" value="">
 
-                <div class="form-group" style="margin:0px 10px;">
-                    <label class="" for="exam_entity_id">Exam</label>
-                    <select id="exam_entity_id" class="form-control" name="ei">
-                        <option value="none">Select an exam..</option>
-                        @foreach($progress->exam() as $option)
-                            <option value="{{ $option['exam_entity_id'] }}">{{ $option['exam'] }}</option>
-                        @endforeach
-                    </select>
+                <div class="form-group">
+                    <label class="col-md-1 control-label" for="exam_entity_id">Exam</label>
+                    <div class="col-md-3">
+                        <select id="exam_entity_id" class="form-control" name="ei">
+                            <option value="none">Select an exam..</option>
+                            @foreach($progress->exam() as $option)
+                                <option value="{{ $option['exam_entity_id'] }}">{{ $option['exam'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group" style="margin:0px 10px;">
-                    <label class="" for="class_entity_id">Class</label>
-                    <select id="class_entity_id" class="form-control" name="ci">
-                        <option value="none">Select a class..</option>
-                        @foreach($progress->classes() as $option)
-                            <option value="{{ $option['class_entity_id'] }}">{{ $option['class_name'] }}</option>
-                        @endforeach
-                    </select>
+
+                <div class="form-group">
+                    <label class="col-md-1 control-label" for="class_entity_id">Class</label>
+                    <div class="col-md-3">
+                        <select id="class_entity_id" class="form-control" name="ci">
+                            <option value="none">Select a class..</option>
+                            @foreach($progress->classes() as $option)
+                                <option value="{{ $option['class_entity_id'] }}">{{ $option['class_name'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <button id="generate-report" style="margin:0px 10px;" class="btn btn-primary" type="submit">Generate Student Progress Report</button>
+
+                <div class="row">
+                    <div class="col-md-3 col-md-offset-1">
+                        <button id="generate-report" class="btn btn-primary" type="submit">Generate Student Progress Report</button>
+                    </div>
+                </div>
             </form>
 
         </div>
@@ -48,6 +58,10 @@
 <script type="text/javascript">
 
     $('#generate-report-form').submit(function() {
+        if (! $('#generate-report-form').valid()) {
+            return false;
+        }
+
         if ($('#exam_entity_id option:selected').val() == 'none') {
             alert('Please select an exam.');
             return false;
@@ -62,5 +76,15 @@
         return true;
     });
 
+    $('#generate-report-form').validate({
+        rules: {
+            ei: {
+                requiredSelect: true
+            },
+            ci: {
+                requiredSelect: true
+            }
+        }
+    });
 </script>
 @endsection
