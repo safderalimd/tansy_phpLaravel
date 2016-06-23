79506e987dac1617184559058fb6f015e05ed014
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-23 16:23:19 +0300

    fix

--- resources/views/emails/exception.blade.php
+++ resources/views/emails/exception.blade.php
@@ -1,20 +1,26 @@
 <br/>
 
-Domain: {{session()->get('dbConnectionData.login_domain')}} <hr/><br/><br/>
+Domain: {{session()->get('dbConnectionData.login_domain')}}
 
-Error message: "{{$exception->getMessage()}}" <hr/><br/><br/>
-Error Line: "{{$exception->getLine()}}" <hr/><br/><br/>
-Error File: "{{$exception->getFile()}}" <hr/><br/><br/>
-Error Code: "{{$exception->getCode()}}" <hr/><br/><br/>
+<hr/>
 
-Session ID: {{session()->get('user.sessionID')}} <hr/><br/><br/>
-User ID: {{session()->get('user.userID')}} <hr/><br/><br/>
-User Security Group: {{session()->get('user.userSecurityGroup')}} <hr/><br/><br/>
-Debug Sproc: {{session()->get('user.debugSproc')}} <hr/><br/><br/>
-Audit Screen Visit: {{session()->get('user.auditScreenVisit')}} <hr/><br/><br/>
-Company Name: {{session()->get('user.companyName')}} <hr/><br/><br/>
-Default Facility Id: {{session()->get('user.defaultFacilityId')}} <hr/><br/><br/>
+Error message: "{{$exception->getMessage()}}" <br/>
+Error Line: "{{$exception->getLine()}}" <br/>
+Error File: "{{$exception->getFile()}}" <br/>
+Error Code: "{{$exception->getCode()}}" <br/>
 
-Error Trace: "{{$exception->getTraceAsString()}}" <hr/><br/><br/>
+<hr/>
+
+Session ID: {{session()->get('user.sessionID')}} <br/>
+User ID: {{session()->get('user.userID')}} <br/>
+User Security Group: {{session()->get('user.userSecurityGroup')}} <br/>
+Debug Sproc: {{session()->get('user.debugSproc')}} <br/>
+Audit Screen Visit: {{session()->get('user.auditScreenVisit')}} <br/>
+Company Name: {{session()->get('user.companyName')}} <br/>
+Default Facility Id: {{session()->get('user.defaultFacilityId')}} <br/>
+
+<hr/>
+
+Error Trace: "{{$exception->getTraceAsString()}}" <br/>
 
 
