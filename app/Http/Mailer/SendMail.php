9e49d2241855c61da748e06f18850f7b85b94f71
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-23 18:09:14 +0300

    Add product owner to erro mail

--- app/Http/Mailer/SendMail.php
+++ app/Http/Mailer/SendMail.php
@@ -10,24 +10,30 @@ class SendMail
 {
     public static function exception(Exception $exception)
     {
-        if (App::environment('local')) {
-            return;
-        }
-
-        $admin = env('ADMIN_EMAIL');
-        if (! $admin) {
-            return;
-        }
-
-        $env = App::environment();
-        $env = strtoupper($env);
-
         try {
 
-            Mail::send('emails.exception', ['exception' => $exception], function ($m) use ($admin, $env) {
-                $m->to($admin, 'Admin');
+            if (App::environment('local')) {
+                return;
+            }
+
+            $env = App::environment();
+            $env = strtoupper($env);
+
+            // send mail to product owner
+            Mail::send('emails.exception', ['exception' => $exception], function ($m) use ($env) {
+                $m->to('safderalimd@outlook.com', 'Admin');
                 $m->subject("TansyCloud {$env} Error Message!");
             });
+
+            // send mail to developer admin
+            $admin = env('ADMIN_EMAIL');
+            if ($admin) {
+                Mail::send('emails.exception', ['exception' => $exception], function ($m) use ($admin, $env) {
+                    $m->to($admin, 'Admin');
+                    $m->subject("TansyCloud {$env} Error Message!");
+                });
+            }
+
 
         } catch (Exception $e) {
 
