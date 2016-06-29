b6187636f9d00f9fa4657a199fb907c4f6c0a592
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-06-29 15:45:50 +0300

    fix

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -9,5 +9,5 @@
 <link type="text/css" href="/bower_components/bootstrap-checkbox-x/css/checkbox-x.min.css" rel="stylesheet">
 
 <link href="/css/simple-sidebar.css?v=1" rel="stylesheet">
-<link href="/css/custom.css?v=6" rel="stylesheet" type="text/css">
+<link href="/css/custom.css?v=7" rel="stylesheet" type="text/css">
 
