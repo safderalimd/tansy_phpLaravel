50fabfc60974c46433f8cb22860e3d7a77f32702
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-08-11 16:36:44 +0300

    improve

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,17 +1,16 @@
 @include('include.meta-and-title')
 
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="/css/app.css" rel="stylesheet">
 
 <link href="/css/datapicker.css" rel="stylesheet">
 <link href="/css/datatables-bootstrap3.css" rel="stylesheet">
 <link href="/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<link href="/css/bootstrap-select.css" rel="stylesheet">
 
 <link href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 <link href="/bower_components/bootstrap-combobox/css/bootstrap-combobox.css" rel="stylesheet">
 <link href="/bower_components/bootstrap-checkbox-x/css/checkbox-x.min.css" rel="stylesheet">
 <link href="/bower_components/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
 <link href="/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-<link href="/css/bootstrap-select.css" rel="stylesheet">
 
 <link href="{{ elixir('css/elixir.css') }}" rel="stylesheet">
