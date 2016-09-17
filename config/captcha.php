8b1d8ef0305f2a2d2adefd757a357d69ba76ad58
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-09-18 01:18:16 +0300

    recaptcha

---
+++ config/captcha.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Secret Key
+    |--------------------------------------------------------------------------
+    |
+    | Use this for communication between your site and Google. Be sure to
+    | keep it a secret.
+    |
+    */
+
+    'secret' => '6LeTAAcUAAAAANdZ5NVuotperFVQUPZ1zTFp4X-S',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | Use this in the HTML code your site serves to users.
+    |
+    */
+
+    'sitekey' => '6LeTAAcUAAAAAF2K6Yj_keMTQNjqsvmywR1I2HT6',
+
+];
