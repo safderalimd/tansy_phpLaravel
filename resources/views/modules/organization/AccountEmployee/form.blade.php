8745f375032e0e8a9b926c46e55939b892ee891a
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-10-19 22:33:35 +0300

    fix

--- resources/views/modules/organization/AccountEmployee/form.blade.php
+++ resources/views/modules/organization/AccountEmployee/form.blade.php
@@ -238,8 +238,8 @@
 <hr/>
 <div class="row"><div class="col-md-3 pull-left"><h3>Login</h3></div></div>
                         
-                        <input type="text" name="tmp-name" style="display:none">
-                        <input type="password" name="tmp-pass" style="display:none">
+                        <input type="text" name="tmp-name" placeholder="do not fill" style="position:absolute; left:-1000px; top:-1000px;">
+                        <input type="password" name="tmp-pass" placeholder="do not fill" style="position:absolute; left:-1000px; top:-1000px;">
 
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="login_name">Login Name</label>
