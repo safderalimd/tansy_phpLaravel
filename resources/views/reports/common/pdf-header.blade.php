42cd1104e85acd0de3d361de253d6d7b430cf120
Adam Ludovic <adam.ludovic.m@gmail.com>
2016-10-19 21:27:11 +0300

    fix

--- resources/views/reports/common/pdf-header.blade.php
+++ resources/views/reports/common/pdf-header.blade.php
@@ -1,8 +1,10 @@
 <div class="row">
+{{--
     <div class="col-md-4 logo-container">
-        @include('reports.common.pdf-logo-img')
+        include('reports.common.pdf-logo-img')
     </div>
-    <div class="col-md-4 school-container">
+ --}}    
+    <div class="col-md-12 school-container">
         <h3 class="school-name text-center">{{$school}}</h3>
         <h4 class="school-phone text-center">{{$line2}}</h4>
         <h4 class="school-phone text-center">{{$line3}}</h4>
